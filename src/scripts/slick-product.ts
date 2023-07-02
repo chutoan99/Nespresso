29bff56632e92fc0b073ef99c4a8be1db300afcf
chutoan99 <truongvanchutoan1999@gmamil.com>
2023-07-03 01:36:43 +0700

    add html

---
+++ src/scripts/slick-product.ts
@@ -0,0 +1,24 @@
+// $(".accessories-list").slick({
+//   dots: false,
+//   infinite: false,
+//   slidesToShow: 3,
+//   slidesToScroll: 3,
+//   responsive: [
+//     {
+//       breakpoint: 1024,
+//       settings: {
+//         arrows: false,
+//         slidesToShow: 2,
+//         slidesToScroll: 2,
+//       },
+//     },
+//     {
+//       breakpoint: 768,
+//       settings: {
+//         arrows: false,
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//       },
+//     },
+//   ],
+// });
